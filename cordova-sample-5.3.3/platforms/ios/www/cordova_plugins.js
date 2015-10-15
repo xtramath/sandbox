31a996af570dfaccd87b2013d40dfb105f52eafc
xtramath <david@xtramath.org>
2015-10-14 17:43:01 -0700

    cordova plugin add cordova-plugin-splashscreen

--- cordova-sample-5.3.3/platforms/ios/www/cordova_plugins.js
+++ cordova-sample-5.3.3/platforms/ios/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-device": "1.0.1"
+    "cordova-plugin-device": "1.0.1",
+    "cordova-plugin-splashscreen": "2.1.0"
 }
 // BOTTOM OF METADATA
 });
